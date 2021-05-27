99762ce13fb0210493ff9da9a8b8f6616d9e9fd3
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-05-27 00:26:45 -0300

    Refatorando IF

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,21 +3,23 @@ function welcome() {
 
     let userName = document.getElementById('userName').value
     let userPassword = document.getElementById('userPassword').value
-    let userList = ['David', 'Atila', 'Raphael']
+    let userList = ['David', 'Atila', 'Raphael', 'Luis']
 
-    if(userName == "" || userPassword == ""){
-        alert('Você não digitou o seu nome ou sua senha, favor, faça isto antes de continuar.')        
-    } 
+    if (!userName || !userPassword) {
+        alert('Você não digitou o seu nome ou sua senha, favor, faça isto antes de continuar.')
+    }
     else {
         if (userList.indexOf(userName) != -1) {
             console.log("Usuário cadastrado!");
             alert('Saudações, ' + userName + '.')
             window.location.href = "./assets/page/cadastroLivro.html";
-        } 
+            document.getElementById('userName').value = ""
+            document.getElementById('userPassword').value = ""
+        }
         else {
             alert("Este usuário não existe em nossa base de dados.");
             document.getElementById('userName').value = ""
             document.getElementById('userPassword').value = ""
-        }       
+        }
     }
 }
